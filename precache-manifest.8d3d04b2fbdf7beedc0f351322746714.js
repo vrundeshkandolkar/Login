21b5b6c460fe5258ef40701c4b535e7fefc5a338
vrundesh-bidchat <vrundesh@Bidchats-Mac-mini-3.local>
2019-04-08 13:05:24 +0530

    Updates

---
+++ precache-manifest.8d3d04b2fbdf7beedc0f351322746714.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Login/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "562edb01e3b5f8d3cc8b",
+    "url": "/Login/static/js/runtime~main.bf058e2e.js"
+  },
+  {
+    "revision": "184198ede3d9f8ac9e4e",
+    "url": "/Login/static/js/main.92b4e256.chunk.js"
+  },
+  {
+    "revision": "fa2c7b75fb97cabd0dfb",
+    "url": "/Login/static/js/2.aff0acb6.chunk.js"
+  },
+  {
+    "revision": "184198ede3d9f8ac9e4e",
+    "url": "/Login/static/css/main.41f9d99d.chunk.css"
+  },
+  {
+    "revision": "fa2c7b75fb97cabd0dfb",
+    "url": "/Login/static/css/2.adb810dc.chunk.css"
+  },
+  {
+    "revision": "8fee24dc4120016c93d5aa520d3886fb",
+    "url": "/Login/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/login/precache-manifest.891e91d5a8b8f369f9f48982115812ed.js"
+  "/Login/precache-manifest.8d3d04b2fbdf7beedc0f351322746714.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/login/index.html", {
+workbox.routing.registerNavigationRoute("/Login/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
