5eb9f5e8af8963a06828b5778195a13a81373b61
vrundeshkandolkar <vrundesh.kandolkar@gmail.com>
2019-04-07 21:38:47 -0700

    Updates

---
+++ precache-manifest.9f7f59727c339e57403572be8bc568f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/login/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "d036c0eadd0ee639ddc1",
+    "url": "/login/static/js/runtime~main.299f3dfc.js"
+  },
+  {
+    "revision": "c302a87ed1a36f399c7b",
+    "url": "/login/static/js/main.37f200db.chunk.js"
+  },
+  {
+    "revision": "58597ae328e339b1993e",
+    "url": "/login/static/js/2.aff0acb6.chunk.js"
+  },
+  {
+    "revision": "c302a87ed1a36f399c7b",
+    "url": "/login/static/css/main.7f948caf.chunk.css"
+  },
+  {
+    "revision": "58597ae328e339b1993e",
+    "url": "/login/static/css/2.e9a18239.chunk.css"
+  },
+  {
+    "revision": "c83ea1e38867ab17319c8c34c2e39d82",
+    "url": "/login/index.html"
+  }
+];
